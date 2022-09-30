3293a282c63e19f41fdba54a070b9b0a20e1fe7f
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-30 11:15:23 +0200

    Add project files.

---
+++ uppgift 3.15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_3._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många timmar tänker du hyra bilen för? svaren måste var helt antal timmar" + "80kr per timme ");
+            int timmar= int.Parse(Console.ReadLine());
+            {
+                if (timmar > 950) ;
+                Console.WriteLine("du kan tyvär inte hyra bilen för mer än 950 kr");
+               
+            }
+            {  
+              else if (timmar < 950)
+                Console.WriteLine("priset är totalt" + timmar * 80 + "kr");
+            }
+
+
+
+        }
+    }
+}
